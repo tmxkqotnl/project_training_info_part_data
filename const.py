0690197e4699adcce65721f921c5f958bf10c70c
tmxkqotnl <tmxkqotnl@gmail.com>
2022-05-11 23:22:34 +0900

    fix, 공통코드 추가

--- const.py
+++ const.py
@@ -1,6 +1,21 @@
 URLS = {
-        'training_list':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp',
-        'agency_info':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_2.jsp',
-        'schedule':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_3.jsp',
-        'attendence':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_4.jsp'
+    "training_list": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp",
+    "agency_info": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_2.jsp",
+    "schedule": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_3.jsp",
+    "attendence": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_4.jsp",
+}
+
+COMMON_CODE = {
+    "훈련지역 대분류 코드": "00",
+    "훈련지역 중분류 코드": "01",
+    "KECO 대분류 코드": "02",
+    "KECO 중분류 코드": "03",
+    "KECO 소분류 코드": "04",
+    "NCS 대분류 코드": "05",
+    "NCS 중분류 코드": "06",
+    "NCS 소분류 코드": "07",
+    "NCS 세분류 코드": "08",
+    "훈련종류 코드": "09",
+    "훈련방법 코드": "10",
+    "훈련기관 구분코드": "11",
 }
