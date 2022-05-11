0c7191fd4926f6b95ee51b514964fa682b72d20c
tmxkqotnl <tmxkqotnl@gmail.com>
2022-05-11 17:44:54 +0900

    feat, URLs

---
+++ const.py
@@ -0,0 +1,6 @@
+URLS = {
+        'training_list':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp',
+        'agency_info':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_2.jsp',
+        'schedule':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_3.jsp',
+        'attendence':'https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_4.jsp'
+}
