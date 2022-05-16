2fc47169bf125a163a523cee877bc43a4db70159
tmxkqotnl <tmxkqotnl@gmail.com>
2022-05-16 17:25:38 +0900

    refac, 리팩토링으로 인한 제거

--- controller/__init__.py
+++
@@ -1,8 +0,0 @@
-# import sys
-# from os.path import dirname, realpath
-
-# parent_dir = dirname(realpath(__file__))
-# sys.path.append(parent_dir)
-
-from .training_list_controller import get_list, get_all_training_list
-from .url_controller import get_full_url, get_api_response
