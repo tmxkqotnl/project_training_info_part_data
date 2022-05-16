46ffd4a48614237c6d81c9c5a009cec85fb23a35
tmxkqotnl <tmxkqotnl@gmail.com>
2022-05-16 17:26:05 +0900

    feat, 에러로깅 && fix, 파라미터 검사

--- libs.py
+++ libs.py
@@ -1,7 +1,40 @@
+import traceback
+import logging
+
+import requests
+
+logger = logging.getLogger(__name__)
+
 def dict_to_query(d: dict[str, str]):
+    if len(d) == 0:
+        return ''
+    
     items = d.items()
     return "&".join(list(map(lambda x: "=".join(x), items)))
 
+def logging_msg(exception_obj,msg):
+    logging.error(msg)
+    logging.debug(exception_obj)
+    
+def error_handler(func):
+    def inner(*args, **kargs):
+        try:
+            return func(*args, **kargs)
+        except TypeError as te:
+            logging_msg(te,"TypeError Occured. Check Your Arguments or Variables ")
+        except KeyError as ke:
+            logging_msg(ke,"KeyError Occurred. Invalid Key is used")
+        except ValueError as ve:
+            logging_msg(ve,"Variable Type is not appropriate or None Type")
+            return None
+        except requests.exceptions.RequestException as re:
+            logging_msg(re,"Requests Error Occured.")
+            return None
+        except Exception as e:
+            logging_msg(e,"EXCEPTION HAS NO HANLDER")
+        finally:
+            logging.debug("Error Handling.. find me")
+            logging.debug(traceback.format_exc())
 
-
+    return inner
 
