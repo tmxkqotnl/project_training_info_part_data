f64a3f37eaa4917c792833046251e95450a881b6
tmxkqotnl <tmxkqotnl@gmail.com>
2022-05-16 17:25:23 +0900

    feat, URL, LEVEL 추가, 리팩토링을 위한 상수 추가 등

--- const.py
+++ const.py
@@ -1,26 +1,48 @@
+LOGGING_LEVEL = {"DEV": 10, "STAG": 30, "PROD": 40}
+
 ###########################################################
+
+INFO_TYPE = {"목록": "1", "과정/기관정보": "2", "훈련일정": "3", "훈련생출결정보": "4"}
 
 # HRD
 
-## 구직자훈련과정 API
-
-### URL
+### 구직자훈련과정 URL
 TRAINING_FOR_JOB_HUNTER = {
-    "info_list": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp",  # 구직자훈련과정 목록
-    "agency_info": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_2.jsp",  # 구직자훈련과정 과정/기관정보
-    "schedule_info": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_3.jsp",  # 구직자훈련과정 훈련일정
-    "attendence_info": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_4.jsp",  # 구직자훈련과정 훈련생 출결정보
+    "1": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp",  # 구직자훈련과정 목록
+    "2": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_2.jsp",  # 구직자훈련과정 과정/기관정보
+    "3": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_3.jsp",  # 구직자훈련과정 훈련일정
+    "4": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_4.jsp",  # 구직자훈련과정 훈련생 출결정보
 }
 
-TRAINING_FOR_JOB_HUNTER_AGENCY_INFO_SEARCH_TYPE = {
-    '목록':"default",
-    '시설':"facility_detail",
-    '장비':"eqnm_detail",
+### 근로자 훈련과정 URI
+TRAINING_FOR_WORKER = {
+    "1": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA61/HRDPOA61_1.jsp",
+    "2": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA61/HRDPOA61_2.jsp",
+    "3": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA61/HRDPOA61_3.jsp",
+    "4": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA61/HRDPOA61_4.jsp",
 }
 
-## 공통코드 API
+TRAINING_FOR_ENTERPRISE = {
+    "1": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA62/HRDPOA62_1.jsp",
+    "2": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA62/HRDPOA62_2.jsp",
+    "3": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA62/HRDPOA62_3.jsp",
+}
 
-### URL
+TRAINING_FOR_MIDDLE_AGED = {
+    "1": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA64/HRDPOA64_1.jsp",
+    "1": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA64/HRDPOA64_2.jsp",
+    "1": "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA64/HRDPOA64_3.jsp",
+}
+
+# 과정/기관정보의 정보 타입
+TRAINING_AGENCY_INFO_SEARCH_TYPE = {
+    "목록": "default",
+    "시설": "facility_detail",
+    "장비": "eqnm_detail",
+}
+
+
+### 공통코드 URL
 COMMON_CODE_URL = "https://www.hrd.go.kr/jsp/HRDP/HRDPO00/HRDPOA90/HRDPOA90_1.jsp"
 
 ### 공통코드
